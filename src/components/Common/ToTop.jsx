76cb6f2c81599b71d2fbbf2663eda4d0d512c4fb
chhu1 <1411365142@qq.com>
2016-11-06 16:24:45 +0800

    [newFeature]add toTop

---
+++ src/components/Common/ToTop.jsx
@@ -0,0 +1,48 @@
+import pureRender from 'pure-render-decorator';
+import React, { Component, PropTypes } from 'react';
+
+import './toTop.scss';
+
+@pureRender
+class ToTop extends Component {
+    constructor() {
+        super();
+        this.state = { showToTop: false };
+        this.handleScroll = this.handleScroll.bind(this);
+        this.handleToTopClick = this.handleToTopClick.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll() {
+        let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        this.setState({ showToTop: scrollTop > ToTop.defaultProps.height });
+    }
+
+    handleToTopClick() {
+        document.body.scrollTop && (document.body.scrollTop = 0);
+        document.documentElement.scrollTop && (document.documentElement.scrollTop = 0);
+    }
+
+    render() {
+        const { showToTop } = this.state;
+        if (showToTop) {
+            return (
+                <div className="to-top" onClick={this.handleToTopClick}>返回顶部</div>
+            )
+        }
+        return null;
+    }
+}
+
+ToTop.defaultProps = {
+    height: document.documentElement.clientHeight || document.body.clientHeight
+};
+
+export default ToTop;
